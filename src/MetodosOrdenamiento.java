8553c5cc155c8fd5b04a4d508968fa7069b8a2d6
Mateo <mateomorejon993@gmail.com>
2025-04-29 08:10:10 -0500

    Avanze de 7 metodos

--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class MetodosOrdenamiento {
 
     // Método de burbuja tradicional con errores
-    // Error encontrado:
+    // Error encontrado: EL arreglo retorna un arreglo de enteros vacio linea 21
+    //Solucuion: Cambiar el retorno de la funcion para que retorne el arreglo ordenado
     public int[] burbujaTradicional(int[] arregloOriginal) {
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
 
@@ -18,11 +19,12 @@ public class MetodosOrdenamiento {
                 }
             }
         }
-        return new int[] {};
+        return arreglo;
     }
 
     // Método de burbuja tradicional con errores
-    // Error encontrado:
+    // Error encontrado: Comparador Logico incorrecto en la linea 34 (<)
+    // Solucion: Cambiar el comparador logico de menor a mayor (>) en la linea 34
 
     public int[] burbujaTradicionalSegundo(int[] arregloOriginal) {
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
@@ -30,7 +32,7 @@ public class MetodosOrdenamiento {
         int n = arreglo.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arreglo[i] < arreglo[j]) {
+                if (arreglo[i]  > arreglo[j]) {
                     // Intercambio de elementos
                     // Estas 3 lineas NO DEBEN ser modificadas
                     int temp = arreglo[i];
@@ -45,14 +47,15 @@ public class MetodosOrdenamiento {
     }
 
     // Método de burbuja tradicional con errores
-    // Error encontrado:
+    // Error encontrado: Salida del limite del rango
+    // Solucion: Añadir un n-1.
     public int[] burbujaTradicionalTercero(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
-
+    
         int n = arreglo.length;
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n - i - 1; j++) {  // Cambié el límite de `j`
                 if (arreglo[j] > arreglo[j + 1]) {
                     // Intercambio de elementos
                     int temp = arreglo[j];
@@ -63,9 +66,11 @@ public class MetodosOrdenamiento {
         }
         return arreglo;
     }
+    
 
     // Método de selección con errores
-    // Error encontrado:
+    // Error encontrado: No contiene un return de tipo int
+    // Solucion: Añadir un return arreglo al final del metodo
     public int[] seleccionPrimero(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
@@ -81,19 +86,22 @@ public class MetodosOrdenamiento {
             arreglo[indiceMinimo] = arreglo[i];
             arreglo[i] = smallerNumber;
         }
+        return arreglo;
 
     }
 
-    // Método de selección con errores
-    // Error encontrado:
+    // Método seleccionSegundo
+    // Error: El segundo bucle usaba `j--`, el cual recorría el arreglo hacia atrás.
+    // Solución: Se cambió a `j++` para que el bucle avance hacia adelante.
+
     public int[] seleccionSegundo(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
 
-        for (int i = 0; i < arreglo.length; i++) {
+        for (int i = 0; i < arreglo.length-1; i++) {
             int indiceMinimo = i;
 
-            for (int j = i + 1; j < arreglo.length; j--) {
+            for (int j = i + 1; j < arreglo.length; j++) {
                 if (arreglo[j] < arreglo[indiceMinimo]) {
                     indiceMinimo = j;
                 }
@@ -106,38 +114,47 @@ public class MetodosOrdenamiento {
         return arreglo;
     }
 
-    // Método de selección con errores
-    // Error encontrado:
-    public int[] seleccionTercero(int[] arregloOriginal) {
+   // Método seleccionTercero
+// Error: Se usó dos veces `arreglo[indiceMinimo]` lo que hace que no funcione.
+// Solución: Se corrigió el intercambio usando las tres líneas tradicionales con una variable temporal.
 
-        int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
+public int[] seleccionTercero(int[] arregloOriginal) {
 
-        for (int i = 0; i < arreglo.length - 1; i++) {
-            int indiceMinimo = i;
+    int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
 
-            for (int j = i + 1; j < arreglo.length; j++) {
-                if (arreglo[j] < arreglo[indiceMinimo]) {
-                    indiceMinimo = j;
-                }
+    for (int i = 0; i < arreglo.length - 1; i++) {
+        int indiceMinimo = i;
+
+        for (int j = i + 1; j < arreglo.length; j++) {
+            if (arreglo[j] < arreglo[indiceMinimo]) {
+                indiceMinimo = j;
             }
-
-            int smallerNumber = arreglo[i];
-            arreglo[indiceMinimo] = arreglo[i];
-            arreglo[indiceMinimo] = smallerNumber;
         }
-        return arreglo;
+
+        // Intercambiar los valores entre arreglo[i] y arreglo[indiceMinimo]
+        if (indiceMinimo != i) { // Solo si son distintos
+            int temp = arreglo[i];
+            arreglo[i] = arreglo[indiceMinimo];
+            arreglo[indiceMinimo] = temp;
+        }
     }
 
+    return arreglo;
+}
+
+    
+
     // Método de inserción con errores
-    // Error encontrado:
+    // Error encontrado: el comparador de i>0 no deja que el ciclo se detenga
+    // Solucion: Cambiar el comparador de i>0 a i>=0 para que el ciclo se detenga
     public int[] insercionPrimero(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
         for (int j = 1; j < arreglo.length; j++) {
             int key = arreglo[j];
             int i = j - 1;
-
-            while (i > 0 && arreglo[i] < key) {
+    
+            while (i >= 0 && arreglo[i] > key) {  
                 arreglo[i + 1] = arreglo[i];
                 i--;
             }
@@ -145,6 +162,7 @@ public class MetodosOrdenamiento {
         }
         return arreglo;
     }
+    
 
     // Método de inserción con errores
     // Error encontrado:
